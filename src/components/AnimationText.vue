f9ad96e29c8de7a145d3ee2857884922c9194f31
mafromist <alev.muge@gmail.com>
2025-10-24 01:17:01 +0300

    Refactor AnimationText component for improved readability and consistency

--- src/components/AnimationText.vue
+++ src/components/AnimationText.vue
@@ -1,34 +1,28 @@
 <template>
-  <span class="cd-headline rotate-1">
-    <span class="blc">Creative </span>
-    <span class="cd-words-wrapper">
-      <b
-        :class="text == i ? 'is-visible' : 'is-hidden'"
-        v-for="(skill, i) in skills"
-        :key="i"
-      >
-        {{ skill }}</b
-      >
-    </span>
-  </span>
+	<span class="cd-headline rotate-1">
+		<span class="blc">Creative </span>
+		<span class="cd-words-wrapper">
+			<b :class="text == i ? 'is-visible' : 'is-hidden'" v-for="(skill, i) in skills" :key="i"> {{ skill }}</b>
+		</span>
+	</span>
 </template>
 
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 
-const skills = ["Designer", "Developer", "Freelancer"];
+const skills = ['Graphic Designer', 'Full Stack Developer', 'Business Consultant'];
 const text = ref(0);
 let interval = null;
 
 onMounted(() => {
-  interval = setInterval(() => {
-    text.value = text.value < skills.length - 1 ? text.value + 1 : 0;
-  }, 3000);
+	interval = setInterval(() => {
+		text.value = text.value < skills.length - 1 ? text.value + 1 : 0;
+	}, 3000);
 });
 
 onBeforeUnmount(() => {
-  if (interval) {
-    clearInterval(interval);
-  }
+	if (interval) {
+		clearInterval(interval);
+	}
 });
 </script>
